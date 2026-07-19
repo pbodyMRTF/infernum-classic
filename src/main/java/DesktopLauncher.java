82458a3af319c33f3c619c7a452f57561059a597
pbodyMRTF <blinkybill19802012@gmail.com>
2026-07-19 19:56:54 +0300

    style: change window title

--- src/main/java/DesktopLauncher.java
+++ src/main/java/DesktopLauncher.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class DesktopLauncher {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setTitle("infernum: classic");
+        config.setTitle("infernum-classic");
         config.setWindowedMode(1024, 768);
         new Lwjgl3Application(new Jgame(), config);
     }
